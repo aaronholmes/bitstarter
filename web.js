cb88e430793b8e0161e71a4247a726bab4de63d3
Aaron Holmes <aholmes@pureguru-software.com>
2013-07-05 20:03:10 +0000

    fix - forgot quotes

--- web.js
+++ web.js
@@ -6,9 +6,10 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   // Read in index.html
   // output
-  var filename = "index.html"
+  var filename = "/home/ubuntu/projects/bitstarter/index.html"
   var buffer = ''; 
-
+  console.log("test");
+  response.send("test2");
  fs.readFile(filename, function (err, data) {
     if (err) throw err;
     buffer = new Buffer(data, "utf-8")
